367bc778590cfd81a84e7d63bdfcea80e4031a75
Bartosz Putek <mamfarta@gmail.com>
2024-09-18 21:26:36 +0200

    feature: Setup svelte for new frontend

---
+++ svelte/src/lib/components/CardIcon.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	export let value: string;
+
+	export let width: number = 60;
+	const height: number = width * 2.5;
+
+	let isSelected: boolean = false;
+</script>
+
+<button
+	aria-label={value}
+	style="width:{width}px; height:{height}px;"
+	class:isSelected
+	on:click={() => {
+		isSelected = !isSelected;
+		console.log(isSelected);
+	}}
+>
+	<img alt="" src={`/images/cards/${value.toUpperCase()}.svg`} />
+</button>
+
+<style>
+	button {
+		display: inline-block;
+		padding: 0;
+		margin: 5px;
+		vertical-align: top;
+		cursor: pointer;
+		border: solid;
+		border-width: 1px;
+		border-radius: 12px;
+		overflow: hidden;
+	}
+
+	.isSelected {
+		opacity: 0.2;
+		cursor: not-allowed;
+	}
+
+	button:hover {
+		outline: 3px solid #9ecaed;
+		box-shadow: 0 0 10px 1px #9ecaed;
+	}
+
+	img {
+		display: block;
+		margin: -2px 0px 0 -4px;
+	}
+</style>
---
+++ svelte/src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import CardIcon from '$lib/components/CardIcon.svelte';
+
+	export const CARD_SUITS: string[] = ['s', 'd', 'h', 'c'];
+	export const CARD_RANKS: string[] = [
+		'2',
+		'3',
+		'4',
+		'5',
+		'6',
+		'7',
+		'8',
+		'9',
+		't',
+		'j',
+		'q',
+		'k',
+		'a'
+	];
+</script>
+
+{#each CARD_SUITS as suit}
+	{#each CARD_RANKS as rank}
+		<CardIcon value={`${rank}${suit}`}></CardIcon>
+	{/each}
+{/each}
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
